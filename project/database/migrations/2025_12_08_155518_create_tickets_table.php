50ad3d33d5dc8d1df18928b5e1e9ed7b09b9da8e
Korkin Igor <korkinogotttit@gmail.com>
2025-12-08 23:01:43 +0700

    set up migrations

---
+++ project/database/migrations/2025_12_08_155518_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->string('theme');
+            $table->text('text');
+            $table->foreignId('status_id')->constrained('statuses');
+            $table->dateTime('response_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
